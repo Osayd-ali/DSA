d7bae23bbd495f412afa444c518a89dff06ebb53
Mir Osayd Ali <mali123@depaul.edu>
2026-05-03 14:04:22 -0500

    searchinarr

---
+++ Arrays/searchinarr.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Scanner;
+
+//Find the target element in the array and return it
+public class searchinarr {
+    public static void main(String[] args){
+        int[] a = {2,4,5,6,7,8,9,11};
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the target element you want to search");
+        int target = s.nextInt();
+        //Implementing linear search that is traversing the array from the beginning one by one
+        for(int i=0; i<a.length; i++){
+            if(a[i] == target){
+                System.out.println("The index of target is: "+ i);
+            }
+        }
+    }
+}
